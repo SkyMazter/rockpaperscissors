8c8a81aeffaa0e804d9429b9236ee842e7e51648
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2018-11-08 14:21:33 -0500

    added com player

--- js/script.js
+++ js/script.js
@@ -4,19 +4,45 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+
+
+
+$("#shoot").click(function(){
+//player's choice
+
+let choice = $("#input").val();
+console.log(choice);
+$("#userChoice").text(choice);
+
+// computer's choice
+
 function getRandomInt(min, max) {
-    let a = Math.ceil(min);
-     let b = Math.floor(max);
-     const final = Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-     console.log(final);
-     return final;
-   }
+  let a = Math.ceil(min);
+  let b = Math.floor(max);
+  const final = Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  console.log(final);
+  return final;
+ }
 let comChoice = getRandomInt(0,3);
 console.log(comChoice);
+const options = ['scissors','rock','paper'];
+console.log(options[comChoice]);
+$("#computerChoice").text(options[comChoice]);
 
-let choice = $("#userChoice").val();
-console.log(choice);
+//deciding the winner
 
+if(choice === "scissors" && comChoice === "paper"){
+alert("Human Wins");
+}
+else if(choice === "scissors" && comChoice === "rock"){
+alert("Computer Wins");
+}
+else if(choice === "scissors" && comChoice === "scissors"){
+alert("Tie");
+}
+//test
+// alert("hi");
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
